673ea8984f9aafd0c2bd08db1d5b92e435675aa4
Mikko Rajakangas <mikko.rajakangas@visma.com>
2021-11-10 18:23:39 +0000

    methods

--- lox/memory.c
+++ lox/memory.c
@@ -73,9 +73,16 @@ static void blackenObject(Obj* object) {
 #endif
 
   switch (object->type) {
+    case OBJ_BOUND_METHOD: {
+      ObjBoundMethod* bound = (ObjBoundMethod*)object;
+      markValue(bound->receiver);
+      markObject((Obj*)bound->method);
+      break;
+    }
     case OBJ_CLASS: {
       ObjClass* klass = (ObjClass*)object;
       markObject((Obj*)klass->name);
+      markTable(&klass->methods);
       break;
     }
     case OBJ_CLOSURE: {
@@ -112,6 +119,9 @@ static void freeObject(Obj* object) {
   printf("%p free type %d\n", (void*)object, object->type);
 #endif
   switch (object->type) {
+    case OBJ_BOUND_METHOD:
+      FREE(ObjBoundMethod, object);
+      break;
     case OBJ_CLASS: {
       FREE(ObjClass, object);
       break;
